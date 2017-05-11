01da34aabcfed09e6852e3d34eed63fa823c2ae7
ohjueng <ohjeung@likelion.org>
2017-05-11 20:56:37 +0900

    최종완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+  	@qall = Question.all
+  end
+
+  def new
+  end
+
+  def create
+  	q = Question.new
+  	q.name = params[:db_name]
+  	q.content = params[:db_content]
+  	q.save
   end
 end
